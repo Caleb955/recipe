af1f7aabcdbd13aa40289af54132c72a613a3bd3
SKI <mmekanessen@gmail.com>
2026-07-12 20:37:22 +0100

    I created a new house for the app.py and moved all the static and templates files into the app folder. I also added a simple route to test if the Flask application is running correctly after installing the python virtual environment. It auto updates the port to 3000 when making changes

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!, I am a Flask application running in a Docker container! I costa rica'
+
+if __name__ == '__main__':
+    app.run(debug=True)
