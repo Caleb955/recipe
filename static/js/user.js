3ac30a5fae1e59211e184e10910526684af78aa5
caleb <calebuwem@mail.com>
2026-08-03 13:27:05 +0100

    little update

--- static/js/user.js
+++ static/js/user.js
@@ -1,4 +1,15 @@
-const colors = ["#87cefa", "#d3d3d3", "#90ee90", "#ffa07a", "#ffb6c1", "#f0e68c", "#dda0dd", "#ff69b4", "#00ced1", "#ff4500"];
+const colors = [
+  "#87cefa",
+  "#d3d3d3",
+  "#90ee90",
+  "#ffa07a",
+  "#ffb6c1",
+  "#f0e68c",
+  "#dda0dd",
+  "#ff69b4",
+  "#00ced1",
+  "#ff4500",
+];
 
 const colorPicker = Math.floor(Math.random() * colors.length);
 
